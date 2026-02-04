b1edd834178a709fc0588bd419826a1dfa390163
saranyabca089 <saran16052005@gmail.com>
2026-02-04 14:20:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ document.getElementById("astroForm").addEventListener("submit", async (e) => {
     const messageDiv = document.getElementById("message");
 
     try {
-        const res = await fetch("http://localhost:5678/webhook-test/Astro", {
+        const res = await fetch("https://saranya16.app.n8n.cloud/webhook/Astro", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify(data)
@@ -33,4 +33,5 @@ document.getElementById("astroForm").addEventListener("submit", async (e) => {
         console.error(error);
     }
 });
+
 
